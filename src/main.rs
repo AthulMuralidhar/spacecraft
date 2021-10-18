f57d552daa6a1661a0fe8aa60edf9f596c5c2003
Athul Muralidhar <athul.muralidhar@bricr.com>
2021-10-18 22:56:11 +0200

    hello world commit

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,26 @@
-use amethyst::{
-    core::transform::TransformBundle,
-    input::{InputBundle, StringBindings},
-    prelude::*,
-    renderer::{
-        plugins::{RenderFlat2D, RenderToWindow},
-        types::DefaultBackend,
-        RenderingBundle,
-    },
-    ui::{RenderUi, UiBundle},
-    utils::application_root_dir,
-};
+use amethyst::{prelude::*, utils::application_root_dir};
 
-mod state;
+struct Example;
+
+impl EmptyState for Example {
+    fn on_start(&mut self, _: StateData<'_, ()>) {
+        println!("Begin!");
+    }
+
+    fn on_stop(&mut self, _: StateData<'_, ()>) {
+        println!("End!");
+    }
+
+    fn update(&mut self, _: StateData<'_, ()>) -> EmptyTrans {
+        println!("Hello from Amethyst!");
+        Trans::Quit
+    }
+}
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
-
-    let app_root = application_root_dir()?;
-
-    let resources = app_root.join("assets");
-    let display_config = app_root.join("config/display_config.ron");
-    let key_bindings_path = app_root.join("config/input.ron");
-
-    let game_data = GameDataBuilder::default()
-        .with_bundle(TransformBundle::new())?
-        .with_bundle(
-            InputBundle::<StringBindings>::new().with_bindings_from_file(&key_bindings_path)?,
-        )?
-        .with_bundle(UiBundle::<StringBindings>::new())?
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config)?
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
-                )
-                .with_plugin(RenderUi::default())
-                .with_plugin(RenderFlat2D::default()),
-        )?;
-
-    let mut game = Application::new(resources, state::MyState, game_data)?;
+    let assets_dir = application_root_dir()?.join("assets");
+    let mut game = Application::build(assets_dir, Example)?.build(())?;
     game.run();
 
     Ok(())
