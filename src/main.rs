28a26ad475e2b78dd5e9f86537ec072af82797d2
Athul Muralidhar <35686547+AthulMuralidhar@users.noreply.github.com>
2021-10-18 20:35:24 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use amethyst::{
+    core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    ui::{RenderUi, UiBundle},
+    utils::application_root_dir,
+};
+
+mod state;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+
+    let resources = app_root.join("assets");
+    let display_config = app_root.join("config/display_config.ron");
+    let key_bindings_path = app_root.join("config/input.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            InputBundle::<StringBindings>::new().with_bindings_from_file(&key_bindings_path)?,
+        )?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config)?
+                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                )
+                .with_plugin(RenderUi::default())
+                .with_plugin(RenderFlat2D::default()),
+        )?;
+
+    let mut game = Application::new(resources, state::MyState, game_data)?;
+    game.run();
+
+    Ok(())
+}
