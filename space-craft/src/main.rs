384d38d7ec75ac4b395a1dd6fbb91a69e180f50a
Athul Muralidhar <athul.m68@gmail.com>
2022-02-05 01:48:40 +0100

    porting snake to craft v1

--- space-craft/src/main.rs
+++ space-craft/src/main.rs
@@ -1,3 +1,48 @@
+#[allow(unused_imports)]
+
+use bevy::prelude::*;
+// use bevy::render::pass::ClearColor;
+
+// https://bevy-cheatbook.github.io/programming/app-builder.html
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        // bevy
+        .add_plugins(DefaultPlugins)
+
+        // resources:
+        .insert_resource(ClearColor(Color::rgb(0.04,0.04,0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "craft".to_string(),
+            width: 500.0,
+            height: 500.0,
+            ..Default::default()
+        })
+        .add_startup_system(setup.system())
+        // if it implements `Default` or `FromWorld`
+        // .init_resource::<MyFancyResource>()
+
+        // // events:
+        // .add_event::<LevelUpEvent>()
+
+        // // systems to run once at startup:
+        // .add_startup_system(spawn_player)
+
+        // // systems to run each frame:
+        // .add_system(player_level_up)
+        // .add_system(debug_levelups)
+        // .add_system(debug_stats_change)
+        // ...
+
+        // launch the app!
+        .run();
+}
+
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7,0.7,0.7).into()),
+        food_material: materials.add(Color::rgb(1.0,1.0,1.0).into()),
+        segment_material: materials.add(Color::rgb(0.3,0.3,0.3).into())
+    });
 }
