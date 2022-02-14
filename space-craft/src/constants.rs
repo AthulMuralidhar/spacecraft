91ec586c140253af52bd743fee00d904e6ea3be1
Athul Muralidhar <athul.m68@gmail.com>
2022-02-14 22:19:50 +0100

    cleaned up and resolved comments

--- space-craft/src/constants.rs
+++ space-craft/src/constants.rs
@@ -5,6 +5,4 @@ pub const ARENA_HEIGHT: u32 = 10;
 
 pub struct Materials {
     pub head_material: Handle<ColorMaterial>,
-    pub food_material: Handle<ColorMaterial>,
-    pub segment_material:  Handle<ColorMaterial>
 }
--- space-craft/src/main.rs
+++ space-craft/src/main.rs
@@ -22,22 +22,6 @@ fn main() {
             ..Default::default()
         })
         .add_startup_system(setup.system())
-        // if it implements `Default` or `FromWorld`
-        // .init_resource::<MyFancyResource>()
-
-        // // events:
-        // .add_event::<LevelUpEvent>()
-
-        // // systems to run once at startup:
-        // .add_startup_system(spawn_player)
-
-        // // systems to run each frame:
-        // .add_system(player_level_up)
-        // .add_system(debug_levelups)
-        // .add_system(debug_stats_change)
-        // ...
-
-        // launch the app!
         .run();
 }
 
@@ -45,7 +29,5 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.insert_resource(Materials {
         head_material: materials.add(Color::rgb(0.7,0.7,0.7).into()),
-        food_material: materials.add(Color::rgb(1.0,1.0,1.0).into()),
-        segment_material: materials.add(Color::rgb(0.3,0.3,0.3).into())
     });
 }
