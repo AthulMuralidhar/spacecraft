ccf137888401c1b05f7a30193d3cb5edf234ae1a
Athul Muralidhar <athul@keana.io>
2022-04-10 04:25:15 +0200

    following step by step refactor traditional loop game

--- space-craft/src/main.rs
+++ space-craft/src/main.rs
@@ -2,10 +2,12 @@
 // the project closly follows https://sunjay.dev/learn-game-dev/intro.html
 //https://sunjay.dev/learn-game-dev/opening-a-window.html
 
-    use sdl2::pixels::Color;
+use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -19,15 +21,16 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    // canvas.set_draw_color(Color::RGB(0, 255, 255));
+    // canvas.clear();
+    // canvas.present();
+    // render(Color::RGB(0, 255, 255), &mut canvas);
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        // canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -38,10 +41,18 @@ fn main() -> Result<(), String> {
             }
         }
         // The rest of the game loop goes here...
+        i = (i + 1) % 255;
 
-        canvas.present();
+        render(Color::RGB(i, 255, 255 - i), &mut canvas);
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
     Ok(())
+}
+
+
+fn render(color: Color, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.present();
 }
