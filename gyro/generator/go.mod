507ffb1d931ac8d5f701c791754f03ab809fe069
Athul Muralidhar <athul.m68@gmail.com>
2022-04-18 16:48:35 +0200

    added code from official btcd go examples for coloured coins

--- gyro/generator/go.mod
+++ gyro/generator/go.mod
@@ -2,9 +2,15 @@ module github.com/AthulMuralidhar/generator
 
 go 1.18
 
+require github.com/gcash/bchd v0.18.1
+
 require (
 	github.com/cbergoon/merkletree v0.2.0 // indirect
+	github.com/dchest/siphash v1.2.2 // indirect
+	github.com/gcash/bchlog v0.0.0-20180913005452-b4f036f92fa6 // indirect
+	github.com/gcash/bchutil v0.0.0-20210113190856-6ea28dff4000 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 )
--- gyro/generator/src/openassets.go
+++ gyro/generator/src/openassets.go
@@ -1,12 +1,133 @@
+// https://github.com/OpenAssets/openassets/blob/master/openassets/protocol.py
+
+// main source:
+// https://github.com/btcsuite/btcd/tree/master/txscript
+
+
 package main
 
-func Protocol() {
-	// https://github.com/OpenAssets/openassets/blob/master/openassets/protocol.py
+import (
+	"encoding/hex"
+	"fmt"
 
+	"github.com/gcash/bchd/bchec"
+	"github.com/gcash/bchd/chaincfg"
+	"github.com/gcash/bchd/chaincfg/chainhash"
+	"github.com/gcash/bchd/txscript"
+	"github.com/gcash/bchd/wire"
+	"github.com/gcash/bchutil"
+)
+
+
+func Protocol() {
 	// engine, err := Engine()
 
 	// TODO:
 	// convert python reference implementation into go
+
+
+
+	
 	// make this func as the entry function for that module
 	// enjoy
+
+
+	// Ordinarily the private key would come from whatever storage mechanism
+	// is being used, but for this example just hard code it.
+	privKeyBytes, err := hex.DecodeString("22a47fa09a223f2aa079edf85a7c2" +
+		"d4f8720ee63e502ee2869afab7de234b80c")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	privKey, pubKey := bchec.PrivKeyFromBytes(bchec.S256(), privKeyBytes)
+	pubKeyHash := bchutil.Hash160(pubKey.SerializeCompressed())
+	addr, err := bchutil.NewAddressPubKeyHash(pubKeyHash,
+		&chaincfg.MainNetParams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// For this example, create a fake transaction that represents what
+	// would ordinarily be the real transaction that is being spent.  It
+	// contains a single output that pays to address in the amount of 1 BCH.
+	originTx := wire.NewMsgTx(wire.TxVersion)
+	prevOut := wire.NewOutPoint(&chainhash.Hash{}, ^uint32(0))
+	txIn := wire.NewTxIn(prevOut, []byte{txscript.OP_0, txscript.OP_0})
+	originTx.AddTxIn(txIn)
+	pkScript, err := txscript.PayToAddrScript(addr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	txOut := wire.NewTxOut(100000000, pkScript)
+	originTx.AddTxOut(txOut)
+	originTxHash := originTx.TxHash()
+
+	// Create the transaction to redeem the fake transaction.
+	redeemTx := wire.NewMsgTx(wire.TxVersion)
+
+	// Add the input(s) the redeeming transaction will spend.  There is no
+	// signature script at this point since it hasn't been created or signed
+	// yet, hence nil is provided for it.
+	prevOut = wire.NewOutPoint(&originTxHash, 0)
+	txIn = wire.NewTxIn(prevOut, nil)
+	redeemTx.AddTxIn(txIn)
+
+	// Ordinarily this would contain that actual destination of the funds,
+	// but for this example don't bother.
+	txOut = wire.NewTxOut(0, nil)
+	redeemTx.AddTxOut(txOut)
+
+	// Sign the redeeming transaction.
+	lookupKey := func(a bchutil.Address) (*bchec.PrivateKey, bool, error) {
+		// Ordinarily this function would involve looking up the private
+		// key for the provided address, but since the only thing being
+		// signed in this example uses the address associated with the
+		// private key from above, simply return it with the compressed
+		// flag set since the address is using the associated compressed
+		// public key.
+		//
+		// NOTE: If you want to prove the code is actually signing the
+		// transaction properly, uncomment the following line which
+		// intentionally returns an invalid key to sign with, which in
+		// turn will result in a failure during the script execution
+		// when verifying the signature.
+		//
+		// privKey.D.SetInt64(12345)
+		//
+		return privKey, true, nil
+	}
+	// Notice that the script database parameter is nil here since it isn't
+	// used.  It must be specified when pay-to-script-hash transactions are
+	// being signed.
+	sigScript, err := txscript.SignTxOutput(&chaincfg.MainNetParams,
+		redeemTx, 0, -1, originTx.TxOut[0].PkScript, txscript.SigHashAll,
+		txscript.KeyClosure(lookupKey), nil, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	redeemTx.TxIn[0].SignatureScript = sigScript
+
+	// Prove that the transaction has been validly signed by executing the
+	// script pair.
+	flags := txscript.ScriptBip16 | txscript.ScriptVerifyDERSignatures |
+		txscript.ScriptStrictMultiSig |
+		txscript.ScriptDiscourageUpgradableNops |
+		txscript.ScriptVerifyBip143SigHash |
+		txscript.ScriptVerifySchnorr
+	vm, err := txscript.NewEngine(originTx.TxOut[0].PkScript, redeemTx, 0,
+		flags, nil, nil, -1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := vm.Execute(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Transaction successfully signed")
+
 }
