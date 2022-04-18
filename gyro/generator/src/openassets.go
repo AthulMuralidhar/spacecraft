4403217009a5900ffa877578639318c7c294ab95
Athul Muralidhar <athul.m68@gmail.com>
2022-04-18 11:36:46 +0200

    added todo's for later reference

--- gyro/generator/src/openassets.go
+++ gyro/generator/src/openassets.go
@@ -3,5 +3,10 @@ package main
 func Protocol() {
 	// https://github.com/OpenAssets/openassets/blob/master/openassets/protocol.py
 
-	engine, err := Engine()
+	// engine, err := Engine()
+
+	// TODO:
+	// convert python reference implementation into go
+	// make this func as the entry function for that module
+	// enjoy
 }
