c98a0eabea5a540ece583a4fb4f3054001abb0eb
Athul Muralidhar <athul.m68@gmail.com>
2022-04-18 11:06:10 +0200

    removed zap logger, was too complex for now

--- gyro/generator/src/merkle.go
+++ gyro/generator/src/merkle.go
@@ -8,75 +8,74 @@ import (
 	"os"
 
 	"github.com/cbergoon/merkletree"
-	"go.uber.org/zap"
 )
 
 //TestContent implements the Content interface provided by merkletree and represents the content stored in the tree.
 type TestContent struct {
-  x string
+	x string
 }
 
 //CalculateHash hashes the values of a TestContent
 func (t TestContent) CalculateHash() ([]byte, error) {
-  h := sha256.New()
-  if _, err := h.Write([]byte(t.x)); err != nil {
-    return nil, err
-  }
+	h := sha256.New()
+	if _, err := h.Write([]byte(t.x)); err != nil {
+		return nil, err
+	}
 
-  return h.Sum(nil), nil
+	return h.Sum(nil), nil
 }
 
 //Equals tests for equality of two Contents
 func (t TestContent) Equals(other merkletree.Content) (bool, error) {
-  return t.x == other.(TestContent).x, nil
+	return t.x == other.(TestContent).x, nil
 }
 
-func MakeMerkle(logger *zap.Logger) ([]byte, error){
-  //Build list of Content to build tree
-  var list []merkletree.Content
+func MakeMerkle() ([]byte, error) {
+	//Build list of Content to build tree
+	var list []merkletree.Content
 
-  // find all the files in the current directory
-  files, err := ioutil.ReadDir("./")
-    if err != nil {
-      logger.Error(err.Error())
-      return nil, err
-  }
+	// find all the files in the current directory
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Panic(err)
+		return nil, err
+	}
 
-  for _, f := range files {
-    fileName:= f.Name()
-    dat, err := os.ReadFile(fileName)
-    if err != nil {
-      logger.Error(err.Error())
-      return nil, err
-  }
-  logger.Info("printing binary data")
-    fmt.Print(string(dat))
-    // append to list
-    list = append(list, TestContent{x:string(dat)})
-  }
-  
-  //Create a new Merkle Tree from the list of Content
-  t, err := merkletree.NewTree(list)
-  if err != nil {
-    logger.Error(err.Error())
-    return nil, err
-  }
-  
-  //Get the Merkle Root of the tree
-  mr := t.MerkleRoot()
-  logger.Info("printing merkle root")
-  log.Println(mr)
-  
-  //Verify the entire tree (hashes for each node) is valid
-  vt, err := t.VerifyTree()
-  if err != nil {
-    logger.Error(err.Error())
-    return nil, err
-  }
-  log.Println("Verify Tree: ", vt)
-  
-  //String representation
-  log.Println(t)
-  // return merkel 
-  return mr, nil
+	for _, f := range files {
+		fileName := f.Name()
+		dat, err := os.ReadFile(fileName)
+		if err != nil {
+			log.Panic(err.Error())
+			return nil, err
+		}
+		log.Println("printing binary data")
+		fmt.Print(string(dat))
+		// append to list
+		list = append(list, TestContent{x: string(dat)})
+	}
+
+	//Create a new Merkle Tree from the list of Content
+	t, err := merkletree.NewTree(list)
+	if err != nil {
+		log.Panic(err.Error())
+		return nil, err
+	}
+
+	//Get the Merkle Root of the tree
+	mr := t.MerkleRoot()
+	log.Println("printing merkle root")
+	log.Println(mr)
+
+	//Verify the entire tree (hashes for each node) is valid
+	vt, err := t.VerifyTree()
+	if err != nil {
+		log.Panic(err.Error())
+		return nil, err
+	}
+	log.Println("Verify Tree: ", vt)
+
+	//String representation
+	log.Println(t)
+	// return merkel
+	return mr, nil
 }
--- gyro/generator/src/merkle_test.go
+++ gyro/generator/src/merkle_test.go
@@ -9,12 +9,12 @@ func TestMakeMerkle(t *testing.T) {
 		// TODO: Add test cases.
 		// TODO: verify each content of the merkel tree
 		// ex:
-		//
-		// Verify a specific content in in the tree
-		//   vc, err := t.VerifyContent(list[0])
-		//   if err != nil {
-		// log.Fatal(err)
-		//   }
+		// 
+  		// Verify a specific content in in the tree
+//   vc, err := t.VerifyContent(list[0])
+//   if err != nil {
+    // log.Fatal(err)
+  }
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
