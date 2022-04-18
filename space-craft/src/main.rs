1df43a2d7094628627b091b1aa840f738ee2271e
Athul Muralidhar <athul.m68@gmail.com>
2022-04-18 13:36:06 +0200

    added movement to reaper

--- space-craft/src/main.rs
+++ space-craft/src/main.rs
@@ -6,7 +6,7 @@
 // https://sunjay.dev/learn-game-dev/rendering-an-image.html
 // https://sunjay.dev/learn-game-dev/single-sprite.html
 // https://sunjay.dev/learn-game-dev/refactor-player-struct.html
-
+// https://sunjay.dev/learn-game-dev/simple-movement.html
 
 use sdl2::pixels::Color;
 use sdl2::event::Event;
@@ -19,6 +19,33 @@ use sdl2::render::Texture;
 use sdl2::rect::Rect;
 use sdl2::rect::Point;
 
+
+struct Player {
+    position: Point,
+    sprite: Rect,
+    speed: i32,
+}
+
+
+fn render(canvas: &mut Canvas<Window>,
+    color: Color, 
+    texture: &Texture,
+    player: &Player,
+) -> Result<(), String> {
+
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    let (width, height) = canvas.output_size()?;
+
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32/ 2);
+    let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player. sprite.height());
+    
+
+    canvas.copy(texture, player.sprite, screen_rect)?;
+    canvas.present();
+    Ok(())
+}
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -38,29 +65,52 @@ fn main() -> Result<(), String> {
     // image size in px:  312px by 288px
     let texture = texture_creator.load_texture("assets/reaper.png")?;
 
-    // world co-ordinates - normalized cartician at orgin == centre of screen
-    let position = Point::new(0, 0);
-    //  each sprite fits into a  26px by 36px rectangle.
-    let sprite = Rect::new(0,0,26,36);
+    // init player position
+    let mut player = Player {
+        // world co-ordinates - normalized cartician at orgin == centre of screen
+        position: Point::new(0,0),
+        //  each sprite fits into a  26px by 36px rectangle.
+        sprite: Rect::new(0,0,26,36),
+        // 0-10,
+        speed: 5,
+    };
 
+    // main game loop
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
         // canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
         // canvas.clear();
         for event in event_pump.poll_iter() {
+            // A nuanced thing about this is that we're handling all the queued events at once. 
+            // ie. parallelly processed
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running;
                 },
+                Event::KeyDown {keycode: Some(Keycode::Left), ..} => {
+                    // move from centre
+                    player.position = player.position.offset(-player.speed,0);
+
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
+                    player.position = player.position.offset(player.speed, 0);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
+                    player.position = player.position.offset(0, -player.speed);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
+                    player.position = player.position.offset(0, player.speed);
+                },
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
+        // the update
         i = (i + 1) % 255;
 
-        render( &mut canvas, Color::RGB(i, 255, 255 - i), &texture, sprite, position)?;
+        // the renderr
+        render( &mut canvas, Color::RGB(i, 255, 255 - i), &texture, &player)?;
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
@@ -68,23 +118,3 @@ fn main() -> Result<(), String> {
 }
 
 
-fn render(canvas: &mut Canvas<Window>,
-    color: Color, 
-    texture: &Texture,
-    sprite: Rect,
-    position: Point,
-) -> Result<(), String> {
-
-    canvas.set_draw_color(color);
-    canvas.clear();
-
-    let (width, height) = canvas.output_size()?;
-
-    let screen_position = position + Point::new(width as i32 / 2, height as i32/ 2);
-    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
-    
-
-    canvas.copy(texture, sprite, screen_rect)?;
-    canvas.present();
-    Ok(())
-}
