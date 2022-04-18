1f9803734339642c40199211f07ab6297a3a4670
Athul Muralidhar <athul.m68@gmail.com>
2022-04-18 15:17:35 +0200

    added simple hello world template for arduino uno

---
+++ adam/adam/src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+extern crate panic_halt;
+
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    /*
+     * For examples (and inspiration), head to
+     *
+     *     https://github.com/Rahix/avr-hal/tree/main/examples
+     *
+     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
+     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
+     * examples available.
+     */
+
+    let mut led = pins.d13.into_output();
+
+    loop {
+        led.toggle();
+        arduino_hal::delay_ms(1000);
+    }
+}
