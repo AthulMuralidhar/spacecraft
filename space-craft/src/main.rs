a86d9ca5f3d2d8a822f7380e4a1f669a9f43c274
Athul Muralidhar <athul.m68@gmail.com>
2022-05-26 18:44:32 +0200

    added tests - v1

--- space-craft/src/main.rs
+++ space-craft/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
+fn init_game_window() -> Result<i32, io::Error> {
+    return Ok(0);
+}
 fn main() {
-    println!("Hello, world!");
+    init_game_window();
+}
+
+//  TESTS
+#[cfg(test)]
+mod tests {
+    use crate::init_game_window;
+
+    #[test]
+    fn it_renders_the_window() {
+        assert!(init_game_window().is_ok())
+    }
 }
