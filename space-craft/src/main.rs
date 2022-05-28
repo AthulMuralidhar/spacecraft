97e6aa92df9ffade8dcf2affb87395f4ff26d2d3
Athul Muralidhar <athul.m68@gmail.com>
2022-05-28 17:25:56 +0200

    managed to add some code from sdl2

--- space-craft/src/main.rs
+++ space-craft/src/main.rs
@@ -1,10 +1,18 @@
-use std::io;
+use sdl2::VideoSubsystem;
 
-fn init_game_window() -> Result<i32, io::Error> {
-    return Ok(0);
+/*
+The project closely follows https://sunjay.dev/learn-game-dev/intro.html
+
+Extras needed to run on linux:
+- sudo apt-get install libsdl2-dev
+*/
+
+fn init_game_window() -> Result<VideoSubsystem, String> {
+    let video_subsystem = sdl2::init()?.video();
+    return video_subsystem;
 }
 fn main() {
-    init_game_window();
+    init_game_window().ok();
 }
 
 //  TESTS
