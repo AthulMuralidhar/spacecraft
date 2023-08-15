c69468966a3a13b6f0053d3c11987a40ee5971cb
Athul Muralidhar <athul.muralidhar@print.com>
2023-08-15 15:09:27 +0200

    wip on making the ping route work

--- ground-control/spacecraft-site/cmd/main.go
+++ ground-control/spacecraft-site/cmd/main.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+
+	s := &http.Server{
+		Addr:           ":6060",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
+	// test
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	//router.Run() // listen and serve on 0.0.0.0:8080
 }
