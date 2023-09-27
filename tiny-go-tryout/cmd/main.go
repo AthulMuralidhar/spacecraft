0a546dfa519225f5a88808c54e940175da1dbfcc
Athul Muralidhar <athul.muralidhar@print.com>
2023-09-27 16:56:16 +0200

    added todos for tinygo

---
+++ tiny-go-tryout/cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// TODO: get all the lds and make a basic compass functionality
+	// TODO: add tests for said funcionality
+	// TODO: think about servo motros and how that would work
+
+}
