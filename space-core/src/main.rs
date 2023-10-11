86387fdaae99db7f831716623e5b417ce4cc221d
Athul Muralidhar <athul.muralidhar@print.com>
2023-10-11 22:40:40 +0200

    kinda fixed on a board
    * so the current board is the board i need, it has accelerometer and
      gyro capabilities
    * have to still adapt it from the microbit tutorials and apply the
      principles

--- space-core/src/main.rs
+++ space-core/src/main.rs
@@ -1,15 +1,23 @@
+// TODO: use the stm31 discovery controller as base and the embedded rust book to build core functionality
+// TODO: write tests while doing this
+
+
+// TODO: get the current orientation wrt onboard accelerometer
+
+
+// everything i need is in the board used in the rust embedded book: https://docs.rust-embedded.org/book/intro/hardware.html
+
+// i think this should be used:https://docs.rust-embedded.org/discovery/microbit/08-led-compass/take-1.html
+
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+
+use rtt_target::{rprintln, rtt_init_print};
+
 fn main() {
-    // TODO: use the stm32 discovery controller as base and the embedded rust book to build core functionality
-    // TODO: write tests while doing this
-
-
-    // TODO: get the current orientation wrt onboard accelerometer
-
-
-
-    // this is the board: https://www.st.com/en/evaluation-tools/stm32f4discovery.html
-    // here is the crate: https://github.com/stm32-rs/stm32f407g-disc
-
-
+    rtt_init_print!();
+    let board = microbit::Board::take().unwrap();
 
 }
