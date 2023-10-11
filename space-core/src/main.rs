bb18f286b28bb94f89876ba49073d723c2cc7e93
Athul Muralidhar <athul.muralidhar@print.com>
2023-10-11 13:30:44 +0200

    added some accelrometer canditates

--- space-core/src/main.rs
+++ space-core/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    // TODO: use the stm32 discovery controlller as base and the emedded rust book to build core funcitonality
+    // TODO: use the stm32 discovery controller as base and the embedded rust book to build core functionality
     // TODO: write tests while doing this
+
+
+    // TODO: get the current orientation wrt onboard accelerometer
+
+
+    // maybe i should use this one https://www.st.com/resource/en/datasheet/asm330lhh.pdf
+
+    
 }
