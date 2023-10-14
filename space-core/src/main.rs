5ec1aba08cc8061970aeb577eeb2defa9a92a675
Athul Muralidhar <athul.m68@gmail.com>
2023-10-14 23:48:06 +0200

    at least it buils now

--- space-core/src/main.rs
+++ space-core/src/main.rs
@@ -1,23 +1,18 @@
-// TODO: use the stm31 discovery controller as base and the embedded rust book to build core functionality
-// TODO: write tests while doing this
-
-
-// TODO: get the current orientation wrt onboard accelerometer
-
-
-// everything i need is in the board used in the rust embedded book: https://docs.rust-embedded.org/book/intro/hardware.html
-
-// i think this should be used:https://docs.rust-embedded.org/discovery/microbit/08-led-compass/take-1.html
-
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+use cortex_m_rt::entry;
+use panic_halt as _;
+// Just use the HAL for pulling in its interrupt vectors (and may be some other
+// fairy dust).
+use stm32f3xx_hal as _;
 
-use rtt_target::{rprintln, rtt_init_print};
+#[entry]
+fn main() -> ! {
+    let _y;
+    let x = 40;
+    _y = x;
 
-fn main() {
-    rtt_init_print!();
-    let board = microbit::Board::take().unwrap();
-
+    // infinite loop; just so we don't leave this stack frame
+    loop {}
 }
