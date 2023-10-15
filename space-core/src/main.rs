67e29b01b256cdd0ce1eb0c9a68d16fed5c766e4
Athul Muralidhar <athul.m68@gmail.com>
2023-10-15 14:55:04 +0200

    added quickstart generated template

--- space-core/src/main.rs
+++ space-core/src/main.rs
@@ -7,6 +7,13 @@ use panic_halt as _;
 // fairy dust).
 use stm32f3xx_hal as _;
 
+// to run:
+// in one terminal:
+//  openocd -f interface/stlink-v2-1.cfg -f target/stm32f3x.cfg 
+// in the other terminal:
+// gdb-multiarch -q -ex "target remote :3333" target/thumbv7em-none-eabihf/debug/led-roulette
+
+
 #[entry]
 fn main() -> ! {
     let _y;
