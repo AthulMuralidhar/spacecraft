4db7f37ed3d0b23381fc713aeb112de4abfe3056
Athul Muralidhar <athul.m68@gmail.com>
2023-10-15 17:33:15 +0200

    my god figured it out in the end

--- space-core/generated/generated/src/main.rs
+++ space-core/generated/generated/src/main.rs
@@ -7,14 +7,23 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
+// use cortex_m::asm;
 use cortex_m_rt::entry;
+
+
+// to run:
+// in one terminal:
+//  openocd -f interface/stlink-v2-1.cfg -f target/stm32f3x.cfg 
+// in the other terminal:
+// gdb-multiarch -q -ex "target remote :3333" target/thumbv7em-none-eabihf/debug/led-roulette
+
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let _y;
+    let x = 42;
+    _y = x;
 
-    loop {
-        // your code goes here
-    }
+    // infinite loop; just so we don't leave this stack frame
+    loop {}
 }
--- space-core/space-core/src/main.rs
+++ space-core/space-core/src/main.rs
@@ -1,11 +1,15 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+// use cortex_m::asm;
 use cortex_m_rt::entry;
-use panic_halt as _;
-// Just use the HAL for pulling in its interrupt vectors (and may be some other
-// fairy dust).
-use stm32f3xx_hal as _;
+
 
 // to run:
 // in one terminal:
@@ -17,7 +21,7 @@ use stm32f3xx_hal as _;
 #[entry]
 fn main() -> ! {
     let _y;
-    let x = 40;
+    let x = 42;
     _y = x;
 
     // infinite loop; just so we don't leave this stack frame
